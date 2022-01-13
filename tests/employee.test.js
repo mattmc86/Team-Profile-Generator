3436409af23900f610e08e0a6843385b0968d2ac
Matt <86723332+mattmc86@users.noreply.github.com>
2022-01-13 13:18:13 +0000

    Tests added and passing for Employee class

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -3,31 +3,31 @@ const Employee = require('../src/Employee');
 // is it creating instance of object?
 
 test("Is employee creating an object?",()=>{
-    const e = new Employee()
-    expect(typeof(e)).toBe("object")
+    const testEmployee = new Employee()
+    expect(typeof(testEmployee)).toBe("object")
 });
 
-test("Can we add a new name",()=>{
+test("should add a new name",()=>{
     const name = "Matt"
-    const e = new Employee(name)
-    expect(e.name).toBe(name)
+    const testName = new Employee(name)
+    expect(testName.name).toBe(name)
 });
 
-test("Can we add a new ID",()=>{
+test("should add a new ID",()=>{
     const id = "01"
-    const e = new Employee(id)
-    expect(e.id).toBe(id)
+    const testID = new Employee("Matt",id)
+    expect(testID.id).toBe(id)
 });
 
 
-test("Can we add a new email",()=>{
+test("should add a new email",()=>{
     const email = "matt@gmail.com"
-    const e = new Employee(email)
-    expect(e.email).toBe(email)
+    const testEmail = new Employee("Matt","01",email)
+    expect(testEmail.email).toBe(email)
 });
 
-test("Can we select a role",()=>{
-    const role = "Engineer"
-    const e = new Employee(role)
-    expect(e.role).toBe(role)
+test("should select role ",()=>{
+    const role = "Employee"
+    const testRole = new Employee("Matt","01","matt@gmail.com")
+    expect(testRole.getRole()).toBe(role)
 });
